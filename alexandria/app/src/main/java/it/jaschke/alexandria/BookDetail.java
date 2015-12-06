54d9e7f3c6be54b9d423b9b4d9117ba2d0ec9dd3
jirwin-dev <jirwin.dev@gmail.com>
2015-12-06 17:02:55 -0500

    Removed back button, fixed null EAN result

--- alexandria/app/src/main/java/it/jaschke/alexandria/BookDetail.java
+++ alexandria/app/src/main/java/it/jaschke/alexandria/BookDetail.java
@@ -142,11 +142,7 @@ public class BookDetail extends Fragment implements LoaderManager.LoaderCallback
             //Set categories
             String categories = data.getString(data.getColumnIndex(AlexandriaContract.CategoryEntry.CATEGORY));
             ((TextView) rootView.findViewById(R.id.categories)).setText(categories);
-
-            //Handle tablet view
-//            if (rootView.findViewById(R.id.right_container) != null) {
-//                rootView.findViewById(R.id.backButton).setVisibility(View.INVISIBLE);
-//            }
+            
         }
     }
 
